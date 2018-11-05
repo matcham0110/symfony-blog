528ce6534ed3e7e2106424ffe2c02f5f512ecb6c
felaire <felaire@x.com>
2018-11-05 17:03:54 +0100

    Add new comment section to publish content

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -8,8 +8,10 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Article;
+use App\Entity\Comment;
 use App\Repository\ArticleRepository;
 use App\Form\ArticleType;
+use App\Form\CommentType;
 use Doctrine\Common\Persistence\ObjectManager;
 
 class BlogController extends AbstractController
@@ -63,9 +65,24 @@ class BlogController extends AbstractController
     /**
      * @Route("/blog/{id}", name="show")
      */
-    public function show(Article $article) {
+    public function show(Article $article, Request $request, ObjectManager $manager) {
+        $comment = new Comment();
+
+        $form = $this->createForm(CommentType::class, $comment);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment->setCreatedAt(new \Datetime());
+            $comment->setArticle($article);
+
+            $manager->persist($comment);
+            $manager->flush();
+            return $this->redirectToRoute('show', ['id' => $article->getId()]);
+        }
+
         return $this->render('blog/show.html.twig', [
-            'article' => $article
+            'article' => $article,
+            'commentForm' => $form->createView()
         ]);
     }
 }
