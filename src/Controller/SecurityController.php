36d380d72f602e4331124f89e049bcfc178d8f38
felaire <felaire@x.com>
2018-11-05 16:38:27 +0100

    Add login/logout

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -8,13 +8,14 @@ use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class SecurityController extends AbstractController
 {
     /**
      * @Route("/inscription", name="security_registration")
      */
-    public function registration(Request $request, ObjectManager $manager)
+    public function registration(Request $request, ObjectManager $manager, UserPasswordEncoderInterface $encoder)
     {
         $user = new User();
         $form = $this->createForm(RegistrationType::class, $user);
@@ -22,11 +23,30 @@ class SecurityController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $hash = $encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($hash);
             $manager->persist($user);
             $manager->flush();
+            
+            return $this->redirectToRoute("security_login", $user);
         }
         return $this->render('security/registration.html.twig', [
-            'form' => $form->createView(),
+            'form' => $form->createView()
         ]);
+    }
+    /**
+     * @Route("/connexion", name="security_login")
+     */
+    public function login() {
+        return $this->render('security/login.html.twig', [
+            'user' => 'a'
+        ]);
+    }
+
+    /**
+     * @Route("/deconnexion", name="security_logout")
+     */
+    public function deco() {
+        
     }
 }
